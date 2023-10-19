52bafd2ef1f2e51ee0438f12d8529a7099a0c906
Jaymie <jaymie@r3plica.co.uk>
2023-10-19 11:20:22 +0100

    Created some components

---
+++ ui-components/src/lib/atoms/content/content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-storybook-tailwind-nx-content',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss'],
+})
+export class ContentComponent {}
---
+++ ui-components/src/lib/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-storybook-tailwind-nx-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent {}
