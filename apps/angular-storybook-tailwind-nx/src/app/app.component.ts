2a0e41639166290de1d4f71212a2eeeb1e8e15fd
Jaymie <jaymie@r3plica.co.uk>
2023-10-19 14:08:12 +0100

    Added the components to the app

--- apps/angular-storybook-tailwind-nx/src/app/app.component.ts
+++ apps/angular-storybook-tailwind-nx/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutComponent } from '@angular-storybook-tailwind-nx/ui-components/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
+  imports: [NxWelcomeComponent, LayoutComponent],
   selector: 'angular-storybook-tailwind-nx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
