5c6901146596184d018f790794e5727c9962a4db
Jaymie <jaymie@r3plica.co.uk>
2023-10-26 12:46:54 +0100

    Removed the back from the header

--- ui-components-design/templates/header/header.component.ts
+++ ui-components-design/templates/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BadgeComponent } from '../../atoms/badge';
 import { ButtonComponent } from '../../atoms/button';
 
 @Component({
   selector: 'angular-storybook-tailwind-nx-header',
   standalone: true,
-  imports: [CommonModule, BadgeComponent, ButtonComponent],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
