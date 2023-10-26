97ad407a7003c83670155dec78dd0c47614a5998
Jaymie <jaymie@r3plica.co.uk>
2023-10-26 12:38:25 +0100

    Removed the badge from the button, to test tree shaking

--- ui-components-design/atoms/button/button.component.ts
+++ ui-components-design/atoms/button/button.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BadgeComponent } from '../badge';
 
 @Component({
   selector: 'angular-storybook-tailwind-nx-button',
   standalone: true,
-  imports: [CommonModule, BadgeComponent],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
